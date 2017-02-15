fc190015d197ea5585d28d93a8605c70529bc1cf
Deadpool1994 <mhtrami@gmail.com>
2017-02-15 18:25:04 +0530

    read data from firebase using angularfire

--- public/home.js
+++ public/home.js
@@ -1,4 +1,4 @@
-angular.module('cricNotify.home',['ngRoute'])
+angular.module('cricNotify.home',['ngRoute','firebase'])
 
 .config(['$routeProvider', function($routeProvider){
   $routeProvider.when('/',{
@@ -7,6 +7,12 @@ angular.module('cricNotify.home',['ngRoute'])
   });
 }])
 
-.controller('homeCtrl', [function(){
+.controller('homeCtrl', function($scope, $firebaseArray){
   console.log("using home COntroller");
-}]);
+  var ref = firebase.database().ref("/over_example/data_2");
+  // download the data into a local object
+  $scope.syncObject =  $firebaseArray(ref);
+  window.w = $scope.syncObject;
+  console.log($scope.syncObject);
+//  console.log($scope.syncObject.text);
+});
