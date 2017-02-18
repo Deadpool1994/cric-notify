278a3aeb17153a42bfdd18c37c9d910f29f6d222
Deadpool1994 <mhtrami@gmail.com>
2017-02-18 23:30:59 +0530

    Removing some comments

--- server/cronJob.js
+++ server/cronJob.js
@@ -3,14 +3,6 @@ var CronJob = require('cron').CronJob;
 var matchService = require('./matchServices.js');
 const firebase = require('./firebaseAdmin.js')
 
-
-// var get_data = (callback) => {
-//
-//     request('https://query.yahooapis.com/v1/public/yql?q=desc%20cricket.commentary&format=json&diagnostics=true&env=store%3A%2F%2F0TxIGQMQbObzvU4Apia0V0&callback=', function(err,res,body){
-//     callback(body);
-//   });
-// };
-
 var startCRONJob = () => {
   new CronJob('*/30 * * * * *', () => {
 //    matchService.get_scorecard_data(firebase.storeScorecardData);
@@ -24,6 +16,5 @@ var startCRONJob = () => {
 };
 
 module.exports = {
-  // get_data,
   startCRONJob
 };
